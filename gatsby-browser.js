8310e296dc61903d6679832c49deed9e7cbe272b
Muhammad Noor Abdillah Bin Sarifudin <mirana.crew@yahoo.com.sg>
2020-04-15 17:23:33 +0800

    credentials configuration for Storage plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import "./src/styles/site.css"
 import "./src/layouts/layout.css"
-import Amplify, { API } from 'aws-amplify'
+import Amplify, { API, Storage } from 'aws-amplify'
 import awsmobile from './src/aws-exports'
 Amplify.configure(awsmobile)
 API.configure(awsmobile)
+Storage.configure(awsmobile)
