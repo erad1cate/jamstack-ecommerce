0de241b8fae53de97293ca4509ca64c3e7ecc2c1
Muhammad Noor Abdillah Bin Sarifudin <mirana.crew@yahoo.com.sg>
2020-04-15 17:00:52 +0800

    Manually configured API credentials in gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import "./src/styles/site.css"
 import "./src/layouts/layout.css"
-import Amplify from 'aws-amplify'
+import Amplify, { API } from 'aws-amplify'
 import awsmobile from './src/aws-exports'
 Amplify.configure(awsmobile)
+API.configure(awsmobile)
